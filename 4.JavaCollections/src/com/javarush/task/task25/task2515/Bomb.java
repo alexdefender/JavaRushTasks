bfdc7026113cb5b3c2e11718f957831db1302f91
Alex Ivanov <alexiv1102@gmail.com>
2018-05-24 12:26:10 +0300

    Tast Space (11) has done

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/Bomb.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Bomb.java
@@ -1,52 +1,26 @@
 package com.javarush.task.task25.task2515;
 
+/**
+ * Класс для бомбы.
+ */
 public class Bomb extends BaseObject {
     public Bomb(double x, double y) {
         super(x, y, 1);
     }
 
-    @Override
-    public boolean isAlive() {
-        return super.isAlive();
-    }
-
-    @Override
-    public double getX() {
-        return super.getX();
-    }
-
-    @Override
-    public void setX(double x) {
-        super.setX(x);
-    }
-
-    @Override
-    public double getY() {
-        return super.getY();
-    }
-
-    @Override
-    public void setY(double y) {
-        super.setY(y);
-    }
-
-    @Override
-    public double getRadius() {
-        return super.getRadius();
-    }
-
-    @Override
-    public void setRadius(double radius) {
-        super.setRadius(radius);
-    }
-
-    @Override
-    public void move() {
-        y++;
-    }
-
+    /**
+     * Отрисовываем себя на холсте.
+     */
     @Override
     public void draw(Canvas canvas) {
         canvas.setPoint(x, y, 'B');
+    }
+
+    /**
+     * Двигаем себя вниз на один ход.
+     */
+    @Override
+    public void move() {
+        y++;
     }
 }
