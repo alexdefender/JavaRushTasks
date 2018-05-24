02a6f8c6835459928406b62f81ba3bda61c0008c
Alex Ivanov <alexiv1102@gmail.com>
2018-05-24 10:39:01 +0300

    Tast Space (5) has done

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/Bomb.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Bomb.java
@@ -1,4 +1,42 @@
 package com.javarush.task.task25.task2515;
 
 public class Bomb extends BaseObject {
+    public Bomb(double x, double y, double radius) {
+        super(x, y, radius);
+    }
+
+    @Override
+    public boolean isAlive() {
+        return super.isAlive();
+    }
+
+    @Override
+    public double getX() {
+        return super.getX();
+    }
+
+    @Override
+    public void setX(double x) {
+        super.setX(x);
+    }
+
+    @Override
+    public double getY() {
+        return super.getY();
+    }
+
+    @Override
+    public void setY(double y) {
+        super.setY(y);
+    }
+
+    @Override
+    public double getRadius() {
+        return super.getRadius();
+    }
+
+    @Override
+    public void setRadius(double radius) {
+        super.setRadius(radius);
+    }
 }
