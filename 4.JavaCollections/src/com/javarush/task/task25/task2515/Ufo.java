637e64e23d947ea567dd79d25277c32bac00ad5e
Alex Ivanov <alexiv1102@gmail.com>
2018-05-24 13:54:39 +0300

    Tast Space (15) has done

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/Ufo.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Ufo.java
@@ -1,6 +1,10 @@
 package com.javarush.task.task25.task2515;
 
+/**
+ * Класс для НЛО
+ */
 public class Ufo extends BaseObject {
+    //картинка для отрисовки
     private static int[][] matrix = {
             {0, 0, 0, 0, 0},
             {0, 0, 1, 0, 0},
@@ -9,67 +13,41 @@ public class Ufo extends BaseObject {
             {0, 0, 0, 0, 0},
     };
 
+    public Ufo(double x, double y) {
+        super(x, y, 3);
+    }
+
+    /**
+     * Метод рисует свой объект на "канвасе".
+     */
     @Override
     public void draw(Canvas canvas) {
         canvas.drawMatrix(x - radius + 1, y - radius + 1, matrix, 'U');
     }
 
+    /**
+     * Двигаем себя на один ход в случайном направлении.
+     */
     @Override
     public void move() {
-        double dx = Math.random() * 2 - 1; //-1..1
-        double dy = Math.random() * 2 - 1; //-1..1
+        double dx = Math.random() * 2 - 1;
+        double dy = Math.random() * 2 - 1;
 
         x += dx;
         y += dy;
 
-        checkBorders(radius, Space.game.getWidth() - radius + 1, 1, Space.game.getHeight() / 2);
+        checkBorders(radius, Space.game.getWidth() - radius + 1, radius - 1, Space.game.getHeight() / 2 - 1);
 
         int random10 = (int) (Math.random() * 10);
         if (random10 == 0)
             fire();
-
     }
 
+    /**
+     * Стреляем.
+     * Сбрасываем(создаем) одну бомбу прямо под собой.
+     */
     public void fire() {
         Space.game.getBombs().add(new Bomb(x, y + 3));
-    }
-
-    public Ufo(double x, double y) {
-        super(x, y, 3);
-    }
-
-    @Override
-    public boolean isAlive() {
-        return super.isAlive();
-    }
-
-    @Override
-    public double getX() {
-        return super.getX();
-    }
-
-    @Override
-    public void setX(double x) {
-        super.setX(x);
-    }
-
-    @Override
-    public double getY() {
-        return super.getY();
-    }
-
-    @Override
-    public void setY(double y) {
-        super.setY(y);
-    }
-
-    @Override
-    public double getRadius() {
-        return super.getRadius();
-    }
-
-    @Override
-    public void setRadius(double radius) {
-        super.setRadius(radius);
     }
 }
